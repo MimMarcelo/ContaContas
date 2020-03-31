c7f8fc9e28e6a41f9b700cfbdd9288c6f986ed36
Marcelo Júnior <rokermarcelo@gmail.com>
2020-03-31 16:44:50 -0300

    remoção da senha do banco

--- src/Helper/EntityManager.php
+++ src/Helper/EntityManager.php
@@ -35,7 +35,7 @@ class EntityManager
         $conn = array(
             'driver' => 'pdo_mysql',
             'user' => 'root',
-            'password' => 'Senha12#',
+            'password' => '',
             'dbname' => 'contacontas',
         );
 
