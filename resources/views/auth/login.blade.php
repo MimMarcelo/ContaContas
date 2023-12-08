74097dae3acadf12a33537a15151488090882a0a
Marcelo Júnior <rokermarcelo@gmail.com>
2023-12-08 18:30:46 -0300

    Refatorada página de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,38 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.master')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title', 'Login')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('content')
+<form method="POST" action="{{ route('login') }}" class="col-sm-4">
+  @csrf
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+  <!-- Email Address -->
+  <div class="form-group">
+    <label for="email">E-mail</label>
+    <input id="email" class="form-control" type="email" name="email" required autofocus autocomplete="username">
+    {{-- <x-input-error :messages="$errors->get('email')" class="mt-2" /> --}}
+  </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+  <!-- Password -->
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
+    {{-- <x-input-error :messages="$errors->get('password')" class="mt-2" /> --}}
+  </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+  <!-- Remember Me -->
+  <div class="form-check form-switch">
+    <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
+    <label for="remember_me" class="form-check-label">Remember me</label>
+  </div>
+  <input type="submit" class="btn btn-primary" value="Log in">
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</form>
+<div class="mt-4">
+  @if (Route::has('password.request'))
+    <a href="{{ route('password.request') }}">
+      Forgot your password?
+    </a>
+  @endif
+</div>
+@endsection
--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,4 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
-    {{ $value ?? $slot }}
-</label>
+{{-- <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}> --}}
+<label {{ $attributes->merge() }}>{{$value}}</label>
